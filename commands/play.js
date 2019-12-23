adf6896ffbe9e7cf8ea2c95968d214975652a81f
WhyHelloPear <kristian.s.montoya@gmail.com>
2019-12-23 03:57:27 -0700

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,16 +1,20 @@
+const ytdl = require('ytdl-core-discord');
+ 
 module.exports = {
 	name: 'play',
 	description: 'Play!',
 	execute(msg, args) {
-		voiceChannel.join()
-  		.then(connection => {});
 		const ytdl = require('ytdl-core');
 		const streamOptions = { seek: 0, volume: 1 };
 		voiceChannel.join()
-	  	.then(connection => {
-		    const stream = ytdl('https://www.youtube.com/watch?v=XAWgeLF9EVQ', { filter : 'audioonly' });
-		    const dispatcher = connection.playStream(stream, streamOptions);
-	  	})
-	  	.catch(console.error);
+		.then(connection => {
+			const stream = ytdl('https://www.youtube.com/watch?v=XAWgeLF9EVQ', { filter : 'audioonly' });
+			const dispatcher = connection.playStream(stream, streamOptions);
+		})
+		.catch(console.error);
 	},
 };
+
+async function play(connection, url) {
+	connection.play(await ytdl(url), { type: 'opus' });
+}
